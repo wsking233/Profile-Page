eb6113ca4a2b52018a34a728b355b0b4084ce948
Shuai Wang <wsking233@gmail.com>
2024-05-15 15:19:32 +1200

    Update ImageLoader.js

--- william-profile/src/components/ImageLoader.js
+++ william-profile/src/components/ImageLoader.js
@@ -6,25 +6,23 @@ import notFound from '../assets/images/404.jpg';
 
 const ImageNotFound = notFound;
 
-const Freejoas = 'freejoas';
-const ArcadeShooter = 'spacewar';
-const Flatties = 'flatties';
+const Freejoas = require.context(`../assets/images/freejoas`, true, /\.(png|jpe?g|svg)$/);
+const ArcadeShooter = require.context(`../assets/images/spacewar`, true, /\.(png|jpe?g|svg)$/);;
+const Flatties = require.context(`../assets/images/flatties`, true, /\.(png|jpe?g|svg)$/);;
 
-function importImages(floder){
-    const images = require.context(`../assets/images/${floder}`, true, /\.(png|jpe?g|svg)$/);
+function importImages(images){
     return images.keys().map((item, index) => {
         return {
             id: index + 1,
             src: images(item),
-            image: images(item).default,
         };
     });
 }
 
+
 const ImageArcadeShooter = importImages(ArcadeShooter);
 const ImageFreejoas = importImages(Freejoas);
 const ImageFlatties = importImages(Flatties);
-
 
 
 export { ImageArcadeShooter, ImageFreejoas, ImageFlatties, ImageNotFound };
